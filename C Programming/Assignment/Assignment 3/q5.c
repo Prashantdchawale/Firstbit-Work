1862ca87d13b93d894814f1d6a885617076b29a8
Prashantdchawale <prashantdchawale@gmail.com>
2025-07-15 21:39:55 +0530

    Update q5.c

--- C Programming/Assignment/Assignment 3/q5.c
+++ C Programming/Assignment/Assignment 3/q5.c
@@ -1,21 +1,30 @@
-
+//armstrong Number
+#include<stdio.h>
+#include<math.h>
 void main()
- {
-    int n = 153;
-    int original = n;
-    int sum = 0, digit;
-
-    while (n != 0) 
 	{
-        digit = n % 10;         
-        sum =sum+ digit * digit * digit;  
-        n = n / 10;              
-    }
+    int num;
+    printf ("Enter any Number : ");
+    scanf("%d",&num);
+	int sum = 0, digit=0, count=0;
+	int original = num;
 
+    
+    int temp=num;
+    while(temp!=0)
+	{
+    	count++;
+		temp=temp/10;
+	}
+	temp=num;
+    while(temp!=0)
+	{
+        digit = temp % 10;         			
+        sum = sum + pow(digit,count);  
+        temp = temp/10;              
+    }
     if (sum == original)
         printf("Armstrong\n");
     else
         printf("Not Armstrong\n");
-
-    
-}
+	}
