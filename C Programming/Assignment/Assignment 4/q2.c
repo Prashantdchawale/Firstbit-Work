33561b4d1124e85a8419fbe2da7b62acbaa5c310
Prashantdchawale <prashantdchawale@gmail.com>
2025-07-16 23:16:20 +0530

    Update q2.c

--- C Programming/Assignment/Assignment 4/q2.c
+++ C Programming/Assignment/Assignment 4/q2.c
@@ -1,23 +1,23 @@
 //2. Print prime numbers in the given range 1 to n. 
 void  main() 
 {
-    int n;
+    int n, flag = 0;
     printf("Enter a number: ");
     scanf("%d", &n);
 
     printf("Prime numbers between 1 and %d are:\n", n);
 
-    for (int i = 1; i <= n; i++) 
+    for (int i = 2; i <= n; i++) 
 	{
-        int isPrime = 1;
+        int flag = 0;
 
-        for (int j = 2; j <= i / 2; j++) 
+        for (int j = 2; j <= i/2; j++) 
             if (i % j == 0) 
-			{
-                isPrime = 0;
+		{
+                flag = 1;
                 break;
-            }
-        if (isPrime)
+            	}
+        if (flag == 0)
             printf("%d ", i);   
     }
 }
