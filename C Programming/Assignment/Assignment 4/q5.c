be9d25973db702e00eb26a773a679e2bff4fcee8
Prashantdchawale <prashantdchawale@gmail.com>
2025-07-17 00:31:32 +0530

    Update q5.c

--- C Programming/Assignment/Assignment 4/q5.c
+++ C Programming/Assignment/Assignment 4/q5.c
@@ -1,10 +1,14 @@
+#include<stdio.h>
 void main()
 {
-	printf("Press 1 To check number is even or odd \n2 To check number is prime or not\n");
-	printf("3.To check number is pallindrome or not\n4 To check number is positive, negative or zero");
-	printf("\n5.To reverse a number \n6 To find sum of digits\n\n");
+	int choice=0;
+	do
+	{
+	printf("1. To check number is even or odd \n2. To check number is prime or not\n");
+	printf("3. To check number is pallindrome or not\n4. To check number is positive, negative or zero");
+	printf("\n5. To reverse a number \n6. To find sum of digits\n\n");
 	int num;
-	printf("Enter Choice i among  operations:");
+	printf("Enter Choice to : ");
 	scanf("%d",& num);
 	if(num==1)
 	{
@@ -74,206 +78,6 @@ void main()
 			int no;
 			printf("Enter a number to reverse : ");
 			scanf("%d",&no);
-			int r1,r2,r3;
-			int q1,rev;
-			r1=no%10;
-			q1=no/10;
-			r2=q1%10;
-			r3=q1/10;
-			rev=r1*100+r2*10+r3;
-			printf("%d\n",rev);
-	}
-	else if(num==6)
-	{
-			int start,end;
-			printf("Enter Starting Number : ");
-			scanf("%d",&start);
- 			printf("Enter ending Number   : ");
-			scanf("%d",&end);
-			int i=start;
-			while (i<=end)
-			{
-				printf("%d\n",i);    
-				i++;
-			}
-	}
-	else
-void main()
-{
-	printf("Press 1 To check number is even or odd \n2 To check number is prime or not\n");
-	printf("3.To check number is pallindrome or not\n4 To check number is positive, negative or zero");
-	printf("\n5.To reverse a number \n6 To find sum of digits\n\n");
-	int num;
-	printf("Enter Choice i among  operations:");
-	scanf("%d",& num);
-	if(num==1)
-	{
-			int no;
-			printf("Enter the number");
-			scanf("%d",&no);
-			if(no%2==0)
-				printf("even");
-			else
-				printf("odd");	
-	}
-	else if(num==2)
-	{
-			int n;
-		 	printf("Enter a number to check prime or not:");
-    			scanf("%d",&num);
-    
-   			int i = 2, flag=0;
-
-   			while (i < num) 
-			{
-        			if (num % i == 0) 
-			{
-           			flag=1;
-           			break;
-        		}
-       				 i++;
-			}
-   			if (flag == 0 && num > 1)
-        		printf("Prime");
-    			else
-      			printf("Not Prime");
-	}                            
-	
-	else if(num==3)
-	{
-		    	int n;
-		  	printf("Enter a number to check Palindrome :");
-		   	scanf("%d",&n);
-		   	int original = n, reverse = 0;
-		
-		   	while (n > 0) 
-			{
-		        int digit = n % 10;
-		        reverse = reverse * 10 + digit;
-		        n =n/ 10;
-		   	}
-		    	if (original == reverse)
-		       		printf("Palindrome");
-		    	else
-		     		printf("Not Palindrome");
-	}
-	else if(num==4)
-	{
-			int no;
-			printf("Enter to check +,-ve, o number : ");
-			scanf("%d",&no);
-			if(no>0)
-				printf("It is positive number");
-			else if (no<0)
-				printf("It is Negative number ");
-			else if(no==0)
-				printf(" zero number");
-	}
-	else if(num==5)
-	{
-			int no;
-			printf("Enter a number to reverse : ");
-			scanf("%d",&no);
-			int r1,r2,r3;
-			int q1,rev;
-			r1=no%10;
-			q1=no/10;
-			r2=q1%10;
-			r3=q1/10;
-			rev=r1*100+r2*10+r3;
-			printf("%d\n",rev);
-	}
-	else if(num==6)
-	{
-			int start,end;
-			printf("Enter Starting Number : ");
-			scanf("%d",&start);
- 			printf("Enter ending Number   : ");
-			scanf("%d",&end);
-			int i=start;
-			while (i<=end)
-			{
-				printf("%d\n",i);    
-				i++;
-			}
-	}
-	else
-void main()
-{
-	printf("Press 1 To check number is even or odd \n2 To check number is prime or not\n");
-	printf("3.To check number is pallindrome or not\n4 To check number is positive, negative or zero");
-	printf("\n5.To reverse a number \n6 To find sum of digits\n\n");
-	int num;
-	printf("Enter Choice i among  operations:");
-	scanf("%d",& num);
-	if(num==1)
-	{
-			int no;
-			printf("Enter the number");
-			scanf("%d",&no);
-			if(no%2==0)
-				printf("even");
-			else
-				printf("odd");	
-	}
-	else if(num==2)
-	{
-			int n;
-		 	printf("Enter a number to check prime or not:");
-    			scanf("%d",&num);
-    
-   			int i = 2, flag=0;
-
-   			while (i < num) 
-			{
-        			if (num % i == 0) 
-			{
-           			flag=1;
-           			break;
-        		}
-       				 i++;
-			}
-   			if (flag == 0 && num > 1)
-        		printf("Prime");
-    			else
-      			printf("Not Prime");
-	}                            
-	
-	else if(num==3)
-	{
-		    	int n;
-		  	printf("Enter a number to check Palindrome :");
-		   	scanf("%d",&n);
-		   	int original = n, reverse = 0;
-		
-		   	while (n > 0) 
-			{
-		        int digit = n % 10;
-		        reverse = reverse * 10 + digit;
-		        n =n/ 10;
-		   	}
-		    	if (original == reverse)
-		       		printf("Palindrome");
-		    	else
-		     		printf("Not Palindrome");
-	}
-	else if(num==4)
-	{
-			int no;
-			printf("Enter to check +,-ve, o number : ");
-			scanf("%d",&no);
-			if(no>0)
-				printf("It is positive number");
-			else if (no<0)
-				printf("It is Negative number ");
-			else if(no==0)
-				printf(" zero number");
-	}
-	else if(num==5)
-	{
-			int no;
-			printf("Enter a number to reverse : ");
-			scanf("%d",&no);
 			int rev=0;
 			while(no != 0)
 			{
@@ -285,18 +89,24 @@ void main()
 	}
 	else if(num==6)
 	{
-			int start,end;
+			int start,end,sum = 0;
 			printf("Enter Starting Number : ");
-			scanf("%d",&start);
- 			printf("Enter ending Number   : ");
-			scanf("%d",&end);
-			int i=start;
-			while (i<=end)
+			scanf("%d",&start);    
+			printf("Enter ending Number   : ");
+    			scanf("%d",&end);
+			int i = start;
+    			while (i <= end)
 			{
-				printf("%d\n",i);    
-				i++;
-			}
+        		sum = sum+i;
+        		i++;
+    			}
+    			printf("\nSum from %d to %d is : %d\n", start, end, sum);
 	}
 	else
         printf("Invalid input. Please choose 1 to 6 only.\n");
+        
+        printf("\nDo you want to perform another operation? (1 for Yes / 0 for No): ");
+        scanf("%d", &choice);
+
+    } while(choice == 1);
 }
