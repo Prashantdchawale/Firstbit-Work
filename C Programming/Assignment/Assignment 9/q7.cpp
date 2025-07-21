6e7252b67d0b365030cdc1e95f445e8eba6e7b32
Prashantdchawale <prashantdchawale@gmail.com>
2025-07-21 09:08:46 +0530

    Add files via upload

---
+++ C Programming/Assignment/Assignment 9/q7.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+void main ()
+{
+	char str1[20],str2[20];
+	printf("Enter String of str1 : ");
+	gets(str1);
+	printf("Enter String of str2 : ");
+	
+	// 7. strncmp
+    printf("Compare first 5 chars of str1 and str2: %d\n", strncmp(str1, str2, 5));
+}
