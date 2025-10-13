7b9c591c1195126385c9a9f698d306a64b75ecdf
Prashantdchawale <prashantdchawale@gmail.com>
2025-10-14 00:25:49 +0530

    Completed

---
+++ Core Java/Collection (06_Oct)/Comparator/MyStudentnameComparator.java
@@ -0,0 +1,15 @@
+package p3;
+
+import java.util.Comparator;
+
+public class MyStudentnameComparator implements Comparator{
+
+	public int compare(Object o1, Object o2) {
+		// TODO Auto-generated method stub
+		Student s1= (Student)o1;
+		Student s2= (Student)o2;
+		
+		return s1.name.compareTo(s2.name);		//name
+	
+	}
+}
